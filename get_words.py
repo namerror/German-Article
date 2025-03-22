079905825ca5439c4e26f65c2b57105381525393
namerror <leonlong2004@outlook.com>
2025-03-22 02:11:32 -0400

    New words added to the dictionary: "L" for A1-C1

--- get_words.py
+++ get_words.py
@@ -93,10 +93,11 @@ Note: already scraped data include
 "I" for "A1" "A2" "B1" "B2" "C1"
 "J" for "A1" "A2" "B1" "B2" "C1"
 "K" for "A1" "A2" "B1" "B2" "C1"
+"L" for "A1" "A2" "B1" "B2" "C1"
 [Do not scrape too often as the website might block this IP]
 '''
 
-scrape_and_save(URL, "K", "C1", dictionary)
+scrape_and_save(URL, "L", "C1", dictionary)
 
 data = json.dumps(dictionary, indent=4)
 
